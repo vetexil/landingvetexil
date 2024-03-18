92220d9a9572b5acda0e5ba05ca84df4088aa1f0
Jair Hernandez <alexis8070@hotmail.com>
2024-03-18 00:48:12 -0500

    agregando estilos y diseño de vetexil

--- partials/body.php
+++ partials/body.php
@@ -1,10 +1,4 @@
-<header class="masthead">
-    <div class="container">
-        <div class="masthead-subheading">Welcome To Our Studio!</div>
-        <div class="masthead-heading text-uppercase">It's Nice To Meet You</div>
-        <a class="btn btn-primary btn-xl text-uppercase" href="#services">Tell Me More</a>
-    </div>
-</header>
+<?php require 'headerHome.php'; ?>
 <!-- Services-->
 <section class="page-section" id="services">
     <div class="container">
---
+++ partials/headerHome.php
@@ -0,0 +1,7 @@
+<header class="masthead">
+    <div class="container">
+        <div class="masthead-subheading">Bienvenido a Vetexil</div>
+        <div class="masthead-heading text-uppercase">Esto es lo que necesitas</div>
+        <a class="btn btn-primary btn-xl text-uppercase" href="#services">catalogo</a>
+    </div>
+</header>
